728fc17632253382ba3d9ab5d6bd84159d137d04
mattveym <matej.tribula@gmail.com>
2025-03-31 11:21:49 +0200

    false error

--- public/admin/assets/js/google-charts.js
+++ public/admin/assets/js/google-charts.js
@@ -3,8 +3,8 @@
 google.charts.load('current', {
   'packages': ['geochart'],
   // Note: you will need to get a mapsApiKey for your project.
-  // See: https://develo  pers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-  'mapsApiKey': process.env.MAPS_API_KEY
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
 });
 google.charts.setOnLoadCallback(drawRegionsMap);
 
